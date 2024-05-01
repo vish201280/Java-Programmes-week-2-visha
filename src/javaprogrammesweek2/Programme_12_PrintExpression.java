4962209f7ea7df1e3e320fe3c00c3571810315d7
vish201280 <vish.201280@gmail.com>
2024-05-01 22:40:21 +0100

    Java-Programmes-week-2-visha

---
+++ src/javaprogrammesweek2/Programme_12_PrintExpression.java
@@ -0,0 +1,22 @@
+package javaprogrammesweek2;
+
+public class Programme_12_PrintExpression {
+    /*Write a Java program to compute the specified expressions and print the
+output.
+Test Data:
+((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+Expected Output : 2.138888888888889
+ */
+        public static void main(String[] args) {
+            Programme_12_PrintExpression t = new Programme_12_PrintExpression();
+            t.printExpressions();
+        }
+
+        //Printing expression
+        public void printExpressions() {
+            double a = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+            System.out.println(a);
+        }
+
+
+    }
